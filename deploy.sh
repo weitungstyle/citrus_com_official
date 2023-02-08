14f9c53395e90b1f06a1bdb64c50be199a0928e0
Victor Cheng <weitungstyle@gmail.com>
2023-02-08 23:24:27 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,9 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:weicenchen/citrus_official.git master:gh-pages
+git push -f git@github.com:weitungstyle/citrus_com_official.git main:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-# git remote add origin https://github.com/weicenchen/CitrusVueShopAdmin.git
-# git push -f https://github.com/weicenchen/citrus_official.git master:gh-pages
+# git remote add origin https://github.com/weitungstyle/citrus_com_official.git
+# git push -f https://github.com/weitungstyle/citrus_com_official.git master:gh-pages
 
 cd -
